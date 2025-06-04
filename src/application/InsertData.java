13fc29d415361f2e5254115459a2d08565d238a2
Eduardo Queiroz <eduhmq@gmail.com>
2025-06-04 19:22:57 -0300

    return added id

--- src/application/InsertData.java
+++ src/application/InsertData.java
@@ -2,9 +2,7 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -22,7 +20,8 @@ public class InsertData {
                     "INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
                     + "VALUES "
-                    + "(?, ?, ?, ?, ?)");
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
             st.setString(1, "Carl Purple");
             st.setString(2, "carl@gmail.com");
@@ -31,7 +30,16 @@ public class InsertData {
             st.setInt(5, 4);
 
             int rowsAffected = st.executeUpdate();
-            System.out.println("Done! Affected rows: " + rowsAffected);
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            }
+            else {
+                System.out.println("No rows affected");
+            }
         }
         catch (SQLException | ParseException e) {
             e.printStackTrace();
